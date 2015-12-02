4993617492050d5110e42dab44ccf61948d22408
tDo66666 <tdo66666@gmail.com>
2015-12-02 14:58:57 +0000

    Commit from GitHub Desktop
    commit the LocalRepository from GitHub Desktop

---
+++ src/MainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author tDo
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+		
+		SecondClass getCount = new SecondClass();
+		System.out.println("The count result is: " + getCount.count(3,4));
+	}
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,12 @@
+
+public class SecondClass {
+	
+	public SecondClass(){
+		
+	}
+	public int count(int x, int y){
+		int result = x + y;
+		
+		return result;
+	}
+}
